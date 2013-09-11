91675bc287c19ea4293315c657f874940ae2beea
liza183 <leesangkeun@gmail.com>
2013-09-11 10:07:47 -0400

    added little more

---
+++ addall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#add all source codes
+
+git add *.*
+git add csv/*.*
+git add lib/*.*
+git add config/*.*
+git add src/gov/ornl/healthcare/standardization/*.*
+git add src/gov/ornl/healthcare/runtime/*.*
+git add src/gov/ornl/healthcare/dbutils/*.*
+git add src/gov/ornl/healthcare/config/*.*
+git add src/gov/ornl/healthcare/core/*.*
+
+
